e930facb1745a89ee21f79513cc550058f62029e
komangmahendra <komangwp.mahendra@gmail.com>
2019-05-03 00:12:12 +0700

    add users login and register

---
+++ server/helper/helper.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+    generateHashPass(pass){
+        return bcrypt.hashSync(pass, +process.env.SALT)
+    },
+    generateJWT(obj){
+        return jwt.sign({name: obj.name, email:obj.email, id: obj.id}, process.env.JWT_KEY, {expiresIn: 4 * 3600})
+    },
+    compareHash(pass, passDB){
+        return bcrypt.compareSync(pass, passDB)
+    },
+    decodeJWT(token){
+        return jwt.decode(token, process.env.JWT_KEY)
+    }
+}
