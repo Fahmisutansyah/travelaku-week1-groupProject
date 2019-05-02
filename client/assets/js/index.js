a56108194d8822d3e1273319d953907c87e5f8fa
Fahmisutansyah <fahmisutansyah@gmail.com>
2019-05-02 22:38:33 +0700

    login page

---
+++ client/assets/js/index.js
@@ -0,0 +1,30 @@
+
+function onSignIn(googleUser) {
+  let id_token = googleUser.getAuthResponse().id_token;
+  console.log(id_token)
+  $("#login-page").slideUp(2000)
+  $('#sign-out').show()
+}
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+    $("#login-page").fadeIn(3000)
+    $("#sign-out").hide()
+  });
+}
+
+$(document).ready(function () {
+  $('.parallax').parallax();
+  $('.modal').modal();
+  $('#sign-out').hide()
+  $('#welcome').hide()
+  $('#lets-login').hide()
+  $('#login-buttons').hide()
+  $('#welcome').fadeIn(3000)
+  $('#lets-login').fadeIn(3000)
+  $('#login-buttons').fadeIn(3000)
+  $('#sign-out').click(function () {
+    signOut()
+  })
+});
