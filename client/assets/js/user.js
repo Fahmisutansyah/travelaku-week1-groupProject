c270926e1372d9ba02c9316c2646560b9b384221
komangmahendra <komangwp.mahendra@gmail.com>
2019-05-03 14:38:30 +0700

    user register and login done

--- client/assets/js/user.js
+++ client/assets/js/user.js
@@ -1,6 +1,5 @@
-let url = `http://localhost:3000`
 
-function register(){
+function registerUser(){
     const name = $('#name').val()
     const email = $('#email').val()
     const password = $('#password').val()
@@ -11,11 +10,49 @@ function register(){
         data : {name, email, password}
     })
     .done(data =>{
-        console.log(data)
+        Swal.fire({
+            title: 'Register Success',
+            type: 'success'
+          })
     })
     .fail((jhr, textStatus) => {
         console.log(textStatus)
+        Swal.fire({
+            title: 'Error Register!',
+            type: 'error',
+          })
     })
 }
 
+
+function loginUser(){
+    const email = $('#login-email').val()
+    const password = $('#login-password').val()
+
+    $.ajax({
+        url : `${url}/users/login`,
+        method : 'POST',
+        data : {email, password}
+    })
+    .done(data =>{
+        localStorage.setItem('token', data.token)
+        fetchCountry()
+    
+        $("#login-page").slideUp(2000,function(){
+          $("#home").fadeIn(1000)
+        })
+        
+        Swal.fire({
+            title: 'Login Success',
+            type: 'success'
+          })
+    })
+    .fail((jhr, textStatus) => {
+        console.log(textStatus)
+        Swal.fire({
+            title: 'Error Login!',
+            type: 'error',
+          })
+    })
+}
 
