543c89d290b44516d933d25161811669f35280a8
komangmahendra <komangwp.mahendra@gmail.com>
2019-05-03 13:58:04 +0700

    add some feature

--- server/routes/oauth/index.js
+++ server/routes/oauth/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const googlSignIn = require('../../controllers/userGoogle')
 
-router.post('/google-sign-in', googlSignIn)
+router.post('/google-sign-in', googlSignIn.googleSignIn)
 
 module.exports = router
